3ad7f083189a889a428b7c65d825b9f5524fd5ca
adarshrajpkd <60054806+adarshrajpkd@users.noreply.github.com>
2020-01-19 10:11:56 +0530

    Add files via upload
    CPP PROGRAM TO FIND SUM OF TWO NUMBERS

---
+++ SUM.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,sum;
+cout<<"\n Enter Two Values to Find The Sum : ";
+cin>>a>>b;
+sum=a+b;
+cout<<"\n Sum of "<<a<<" and "<<b<<" is "<<sum;
+return 0;
+}
